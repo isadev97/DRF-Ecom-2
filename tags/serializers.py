f9585cea10801d9746119b3bcf4f66aaeae6493e
isadev97 <isa325697@gmail.com>
2023-07-08 20:20:38 +0530

    added different types of serializer for read and write

--- tags/serializers.py
+++ tags/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
 from tags.models import Tags
 
-class TagsSerializer(serializers.ModelSerializer):
+# serializer is used for data validation and tag creating
+# write operation into the database
+class WriteTagsSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Tags
+        fields = ['name',]
+
+# serializer is used to convert orm data into json
+# read operation from the database
+class ReadTagsSerializer(serializers.ModelSerializer):
+    
+    created_at = serializers.DateTimeField(format="%d/%m/%y")
+    
     class Meta:
         model = Tags
         fields = ['id', 'name', 'slug', 'created_at']
